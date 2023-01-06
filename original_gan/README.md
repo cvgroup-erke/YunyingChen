71287ada158a595890f1f34df6012e385b5629e1
Yunying-Chen <yunyingchen.yc@gmail.com>
2023-01-06 14:26:14 +0800

    updated

--- original_gan/README.md
+++ original_gan/README.md
@@ -4,18 +4,13 @@ The Discriminator and Generator network are defined in resnet50.py. ResNet50 is 
 The 1-dimension random noise input is changed into a 3-dimension input [3,7,7].                
 
 
+ 
+Using the original loss in Gan, the generated images are as follows:      
+![ouput](https://github.com/cvgroup-erke/YunyingChen/blob/main/original_gan/imgs/ori_gan1.png)
+![output](https://github.com/cvgroup-erke/YunyingChen/blob/main/original_gan/imgs/ori_gan2.png)
 
-Here are some problems that I have encountered and I have some solutions to them, while training:
-1. If the Discriminator is too big, the loss of Discriminator will easily reach 0 and the loss of generator will always stay high. So I tried to keep the Discriminator network small and the Generator network big.
-2. If the random noise input is too big, the Generator network is hard to train and will gain a high loss. So I tried to keep the 3-dimension input small.  
 
-For the final losses, the loss of Discriminator reached 50 and the loss of Generator reached 0.       
-![loss](https://github.com/cvgroup-erke/YunyingChen/blob/main/original_gan/imgs/loss.png)      
-The generated images are as follows:      
-![ouput](https://github.com/cvgroup-erke/YunyingChen/blob/main/original_gan/imgs/9600.png)
-![output](https://github.com/cvgroup-erke/YunyingChen/blob/main/original_gan/imgs/10000.png)
 
-But here are some problems that are unsolved:                
-During the training process, the losses of Generator and Discriminator will jump from a sudden and the result will suddenly turn bad while the learning rate is really small.         
-![loss](https://github.com/cvgroup-erke/YunyingChen/blob/main/original_gan/imgs/loss1.png)  
-![loss](https://github.com/cvgroup-erke/YunyingChen/blob/main/original_gan/imgs/loss2.png)   
+Changing the loss to Wgan, the generated images are as follows:             
+![ouput](https://github.com/cvgroup-erke/YunyingChen/blob/main/original_gan/imgs/wgan1.png)
+![output](https://github.com/cvgroup-erke/YunyingChen/blob/main/original_gan/imgs/wgan2.png)
---
+++ original_gan/wgan.py
@@ -0,0 +1,132 @@
+# dataset: mnist
+import argparse
+import os
+import numpy as np
+import math
+
+import torchvision.transforms as transforms
+from torchvision.utils import save_image
+
+from torch.utils.data import DataLoader
+from torchvision import datasets
+from torch.autograd import Variable
+
+import torch.nn as nn
+import torch
+
+from generator import Generator
+from discriminator import Discriminator
+from resnet50 import ResNet50_G,ResNet50_D
+os.makedirs("images", exist_ok=True)
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--n_epochs", type=int, default=100, help="number of epochs of training")
+parser.add_argument("--batch_size", type=int, default=64, help="size of the batches")
+parser.add_argument("--lr", type=float, default=0.00005, help="learning rate")
+parser.add_argument("--n_cpu", type=int, default=8, help="number of cpu threads to use during batch generation")
+parser.add_argument("--latent_dim", type=int, default=100, help="dimensionality of the latent space")
+parser.add_argument("--img_size", type=int, default=28, help="size of each image dimension")
+parser.add_argument("--channels", type=int, default=1, help="number of image channels")
+parser.add_argument("--n_critic", type=int, default=5, help="number of training steps for discriminator per iter")
+parser.add_argument("--clip_value", type=float, default=0.01, help="lower and upper clip value for disc. weights")
+parser.add_argument("--sample_interval", type=int, default=400, help="interval betwen image samples")
+opt = parser.parse_args()
+print(opt)
+
+img_shape = (opt.channels, opt.img_size, opt.img_size)
+
+cuda = True if torch.cuda.is_available() else False
+
+# Initialize generator and discriminator
+# generator = Generator()
+# discriminator = Discriminator()
+generator = ResNet50_G()
+discriminator = Discriminator()
+if cuda:
+    generator.cuda()
+    discriminator.cuda()
+
+# Configure data loader
+os.makedirs("./data/mnist", exist_ok=True)
+dataloader = torch.utils.data.DataLoader(
+    datasets.MNIST(
+        "./data/mnist",
+        train=True,
+        download=True,
+        transform=transforms.Compose(
+            [transforms.Resize(opt.img_size),
+             transforms.ToTensor(),
+             transforms.Normalize([0.5], [0.5])]    # [] means channel, 0.5,0.5 means mean & std
+                                                    # => img = (img - mean) / 0.5 per channel
+        ),
+    ),
+    batch_size=opt.batch_size,
+    shuffle=True,
+)
+
+# Optimizers
+optimizer_G = torch.optim.RMSprop(generator.parameters(), lr=opt.lr)
+optimizer_D = torch.optim.RMSprop(discriminator.parameters(), lr=opt.lr)
+
+Tensor = torch.cuda.FloatTensor if cuda else torch.FloatTensor
+
+# ----------
+#  Training
+# ----------
+
+batches_done = 0
+for epoch in range(opt.n_epochs):
+
+    for i, (imgs, _) in enumerate(dataloader):
+
+        # Configure input
+        real_imgs = imgs.type(Tensor)
+
+        # ---------------------
+        #  Train Discriminator
+        # ---------------------
+
+        optimizer_D.zero_grad()
+
+        # Sample noise as generator input
+        # z = Tensor(np.random.normal(0, 1, (imgs.shape[0], opt.latent_dim)))
+        z = Tensor(np.random.normal(0, 1, (imgs.shape[0], 3,7,7)))
+
+        # Generate a batch of images
+        fake_imgs = generator(z).detach()
+        # Adversarial loss
+        loss_D = -torch.mean(discriminator(real_imgs)) + torch.mean(discriminator(fake_imgs))
+
+        loss_D.backward()
+        optimizer_D.step()
+
+        # Clip weights of discriminator
+        for p in discriminator.parameters():
+            p.data.clamp_(-opt.clip_value, opt.clip_value)
+
+        # Train the generator every n_critic iterations
+        if i % opt.n_critic == 0:
+            # -----------------
+            #  Train Generator
+            # -----------------
+
+            optimizer_G.zero_grad()
+
+            # Generate a batch of images
+            gen_imgs = generator(z)
+            # Adversarial loss
+            loss_G = -torch.mean(discriminator(gen_imgs))
+
+            loss_G.backward()
+            optimizer_G.step()
+
+            print(
+                "[Epoch %d/%d] [Batch %d/%d] [D loss: %f] [G loss: %f]"
+                % (
+                epoch, opt.n_epochs, batches_done % len(dataloader), len(dataloader), loss_D.item(), loss_G.item())
+            )
+
+        if batches_done % opt.sample_interval == 0:
+            save_image(gen_imgs.data[:25], "resnet_mlp_wgan/%d.png" % batches_done, nrow=5, normalize=True)
+        batches_done += 1
+
